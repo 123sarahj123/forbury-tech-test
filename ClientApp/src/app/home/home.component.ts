a9f2ced84f47a2ebb377fc84389e1fc43e7f0218
LAPTOP-MFB81J8R\magpie <svjackson@live.com>
2022-11-02 15:18:06 +1300

    Added all of the functionality to populate the table with tier rent brackets, tier rent, and total rent Updated README

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -7,4 +7,144 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   title = "Forbury Commercial Rent Calculator";
+
+  public yearProfiles: YearProfile[] = [];
+
+  public yearOneSales: number = 0;
+  public baseRentYearOneToFive: number = 0;
+  public baseRentYearSixToTen: number = 0;
+  public percentageRentTierOne: number = 0;
+  public percentageRentTierTwo: number = 0;
+  public percentageRentTierThree: number = 0;
+  public compoundingAnnualSalesGrowth: number = 0;
+
+
+  
+  calculateRentProfile() {
+
+    // Range of 10, as we only care about the next 10 years
+    for (let i = 1; i <= 10; i++) {
+
+      let yearProfile = new YearProfile(i) // create year profile for current year
+
+      yearProfile.yearSales = this.calculateYearSales(yearProfile.year);
+      yearProfile.baseRent = this.calculateBaseRent(yearProfile.year);
+
+      //calculate the low-end brackets for each tier of a given yearProfile
+      yearProfile.tierOneLowerBracket = this.calculateTierLowEndBracket(yearProfile, Tier.TierOne);
+      yearProfile.tierTwoLowerBracket = this.calculateTierLowEndBracket(yearProfile, Tier.TierTwo);
+      yearProfile.tierThreeLowerBracket = this.calculateTierLowEndBracket(yearProfile, Tier.TierThree);
+
+      //calculate the rent for each tier of a given yearProfile
+      yearProfile.tierOneRent = this.calculateTierRent(yearProfile, Tier.TierOne);
+      yearProfile.tierTwoRent = this.calculateTierRent(yearProfile, Tier.TierTwo);
+      yearProfile.tierThreeRent = this.calculateTierRent(yearProfile, Tier.TierThree);
+
+      yearProfile.totalPercentageRent = this.calculateTotalPercentageRent(yearProfile);
+
+
+      this.yearProfiles.push(yearProfile)
+    }
+
+  }
+
+  // Takes a Tier enumerator and will return the low end bracket for that tier
+  // Tier one low end bracket: base rent/8%
+  // Tier two low end bracket: base rent/4%
+  // Tier three low end bracket: base rent/2%
+  calculateTierLowEndBracket(yearProfile: YearProfile, tier: Tier) {
+    let tierAmount = 0;
+
+    switch (tier) {
+      case Tier.TierOne:
+        tierAmount = yearProfile.baseRent / 0.08
+        return tierAmount;
+
+      case Tier.TierTwo:
+        tierAmount = yearProfile.baseRent / 0.04
+        return tierAmount;
+
+      case Tier.TierThree:
+        tierAmount = yearProfile.baseRent / 0.02
+        return tierAmount;
+    }
+  }
+
+  // Takes a Tier enumerator and will return the rent for that tier
+  calculateTierRent(yearProfile: YearProfile, tier: Tier) {
+    let tierRent = 0;
+
+    switch (tier) {
+      case Tier.TierOne:
+        tierRent = (yearProfile.tierTwoLowerBracket - yearProfile.tierOneLowerBracket) * (this.percentageRentTierOne * 0.01)
+        return tierRent;
+
+      case Tier.TierTwo:
+        tierRent = (yearProfile.tierThreeLowerBracket - yearProfile.tierTwoLowerBracket) * (this.percentageRentTierTwo * 0.01)
+        return tierRent;
+
+      case Tier.TierThree:
+        tierRent = (yearProfile.yearSales - yearProfile.tierThreeLowerBracket) * (this.percentageRentTierThree * 0.01)
+        return tierRent;
+    }
+  }
+
+  // Takes a year profile and sums the percentage rents of tier 1, 2 and 3
+  calculateTotalPercentageRent(yearProfile: YearProfile) {
+    return yearProfile.tierOneRent + yearProfile.tierTwoRent + yearProfile.tierThreeRent;
+
+  }
+
+  // Takes a year as an input, and calculates the sales based on the given year (as there is
+  // compounding annual sales growth from year 2)
+  calculateYearSales(year: number) {
+    let sales = 0;
+    if (year === 1) {
+      sales = this.yearOneSales;
+    } else {
+      sales = this.yearOneSales * (1 + (this.compoundingAnnualSalesGrowth * 0.01)) ** (year-1)
+    }
+    return sales;
+  }
+
+
+  // Takes a year and returns the base rent for the given year
+  calculateBaseRent(year: number) {
+    let baseRent = 0;
+    if (year <= 5) {
+      baseRent = this.baseRentYearOneToFive;
+    } else {
+      baseRent = this.baseRentYearSixToTen;
+    }
+    return baseRent;
+  }
+
 }
+
+export class YearProfile {
+  year: number;
+
+  constructor(year: number) {
+    this.year = year;
+
+  }
+
+  yearSales: number = 0;
+  baseRent: number = 0;
+  tierOneLowerBracket: number = 0;
+  tierTwoLowerBracket: number = 0;
+  tierThreeLowerBracket: number = 0;
+
+  tierOneRent: number = 0;
+  tierTwoRent: number = 0;
+  tierThreeRent: number = 0;
+
+  totalPercentageRent: number = 0;
+}
+
+enum Tier {
+  TierOne,
+  TierTwo,
+  TierThree
+}
+
--- README.md
+++ README.md
@@ -16,9 +16,18 @@ https://localhost:44405/
 
 # What I'd do next time/if I had more time
 - Write unit tests.
+- Set up functionality to destroy and recreate the table on each time you click the calculate button. Currently you need to refresh, else it will keep appending to the table.
+- Format the table and round the numbers. Right now, it is hard to read, and the table does not rebuild itself
+- Consolidate the table. I left all of the columns there so it would be easier to locate where I went wrong if I made a mistake or misunderstood the assignment
 - Spend more time learning this tech stack (haven't really used Angular/.NET core) so I could better structure the files, and approach this problem in a more modularised fashion (rather than "just get it going"). I ended up loading all functionality into the component.ts, wheras that functionality
-should have been delegated to a controller. 
+should have been delegated to a controller. Also if I knew this stack better, I would have spent time making things pretty :p
 - Apply CSS and styling, because it is very ugly. I chose to focus on functionality first. I apologize for the offensive layout
-- Form validation. Error handling for if a user did not enter a number in the form. I just made the default values 0 for the inputs.
-- Tidy up the application. There is a lot of redundant/placeholder code as I just used the start project to get going
-- More structured git commits. Usually I commit small and commit often
+- Form validation. Error handling for if a user inputted something invalid. I just made the default values 0 for the inputs, and assume the user will put numbers in using the correct format (i.e. 2.5 if 2.5%).
+- Tidy up the application. There is a lot of redundant/placeholder code as I just used the Angular starter project to get going
+- More structured git commits. Usually I commit small and commit often. I also wouldn't be soley commiting to main, and would do each feature on a feature branch
+- Refactor. Helper functions calculateTierRent and calculateTierLowEndBracket could be rolled into one
+
+# Assumptions/Decisions
+- That the compounding sales start from year 2. I.e. the annual sales for year two will have the multiplier of 2.5% applied
+- For simplicity's sake, I only calculated the bottom range for each tier, rather than including an upper/lower range
+- I left all of the columns there so it would be easier to locate where I went wrong if I made a mistake or misunderstood the assignment
